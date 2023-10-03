5d2e027cf01412269631c20870bb445ff0027f12
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-10-03 23:21:43 +0200

    use runtime env and add optional duration field

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,31 @@ mod voice;
 use {
   composer::Director,
   serenity::model::prelude::{AttachmentType, GuildId},
-  std::sync::{Arc, Mutex},
+  std::{
+    env,
+    sync::{Arc, Mutex},
+    time::Duration,
+  },
   warp::Filter,
 };
 
 #[tokio::main]
 async fn main() {
-  let director = Arc::new(Mutex::new(Director::new(48_000, None)));
+  let clip_duration: Option<Duration> = match env::var("DURATION") {
+    Ok(v) => Some(Duration::from_millis(
+      v.parse().expect("Was unable to parse duration!"),
+    )),
+    Err(_) => None,
+  };
 
-  let client = client::DiscordClient::new(env!("TOKEN"), director)
-    .await
-    .expect("Error starting discord client.");
+  let director = Arc::new(Mutex::new(Director::new(48_000, clip_duration)));
+
+  let client = client::DiscordClient::new(
+    &env::var("TOKEN").expect("TOKEN was not provided!"),
+    director,
+  )
+  .await
+  .expect("Error starting discord client.");
 
   let clip = warp::path!("clip" / u64).map(move |guild_id: u64| {
     let parsed_id = GuildId(guild_id.clone());
@@ -54,7 +68,10 @@ async fn main() {
   warp::serve(clip)
     .run((
       [127, 0, 0, 1],
-      env!("PORT").parse().expect("Port is not a number"),
+      env::var("PORT")
+        .expect("PORT was not provided!")
+        .parse()
+        .expect("Port is not a number"),
     ))
     .await;
 }
